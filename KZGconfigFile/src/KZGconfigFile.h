e8dcaebb2aa80185ac2eda822806ba3f915e886f
Zagii <zagawus@interia.pl>
2018-10-15 23:14:43 +0200

    bugfix

--- KZGconfigFile/src/KZGconfigFile.h
+++ KZGconfigFile/src/KZGconfigFile.h
@@ -3,8 +3,8 @@
 #include "Arduino.h"
 #include "FS.h"
 
-#define DEBUG   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
-#ifdef DEBUG    //Macros are usually in all capital letters.
+#define DEBUG_CONFIGFILE   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+#ifdef DEBUG_CONFIGFILE    //Macros are usually in all capital letters.
   #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
   #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
 #else
