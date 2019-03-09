6300406b2ac7ffa7c92f20e464fb1522501ddeed
zagii <zagawus@interia.pl>
2019-03-09 23:06:05 +0100

    poprawki do wersji działającej

--- KZGlan_mqtt/src/KZGlan_mqtt.cpp
+++ KZGlan_mqtt/src/KZGlan_mqtt.cpp
@@ -1,16 +1,16 @@
 #include "KZGlan_mqtt.h"
 
-void KZGlan_mqtt::begin(String name,byte [] mac, IPAddress mqttHostIP, String mqttHost,String mqttUser,String mqttPwd, uint16_t mqttPort, KZGlan_mqttCallback mqttCallback)
+void KZGlan_mqtt::begin(String name,byte * mac, IPAddress mqttHostIP, String mqttHost,String mqttUser,String mqttPwd, uint16_t mqttPort, KZGlan_mqttCallback mqttCallback)
 {
   DPRINTLN("Debug KZGlan_mqtt::begin start"); 
-  String _name=name;
+  _name=name;
   _mac[0]=mac[0];_mac[1]=mac[1];_mac[2]=mac[2];
   _mac[3]=mac[3];_mac[4]=mac[4];_mac[5]=mac[5];
-  String _mojTopicIDSubscribe=name+"/Sub/"; //topic z komunikatami z serwera
-  String _mojTopicIDSubscribeConfig=name+"/ConfigSub/"; //topic z komunikatami z serwera
-  String _mojTopicIDPublish=name+"/Pub/"; // topic z komunikatami do serwera
-  String _mojTopicIDPublishDebug="DebugTopic/"+name+"/"; // topic z komunikatami do serwera
-  String _mojTopicIDPing=name+"/Ping/"; //topic z komukatami keepalive
+  _mojTopicIDSubscribe=name+"/Sub/#"; //topic z komunikatami z serwera
+  _mojTopicIDSubscribeConfig=name+"/ConfigSub/#"; //topic z komunikatami z serwera
+  _mojTopicIDPublish=name+"/Pub/"; // topic z komunikatami do serwera
+  _mojTopicIDPublishDebug="DebugTopic/"+name+"/"; // topic z komunikatami do serwera
+  _mojTopicIDPing=name+"/Ping/"; //topic z komukatami keepalive
   _mqttUsr=mqttUser;
   _mqttPwd=mqttPwd;
   _mqttPort=mqttPort;
@@ -23,11 +23,12 @@ void KZGlan_mqtt::begin(String name,byte [] mac, IPAddress mqttHostIP, String mq
   }else
   {
     _mqttHostDNS=mqttHost;
-    _mqttClient.setServer(_mqttHostDNS, _mqttPort);
+    _mqttClient.setServer(_mqttHostDNS.c_str(), _mqttPort);
   }
   //_mqttClient.setCallback(MQTTcallback); <<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<
-  _mqttCallback = mqttCalback;
-  _mqttClient.set_callback([this] (char* topic, byte* payload, unsigned int length) { this->callback(topic, payload, length); }
+  _mqttCallback = mqttCallback;
+  _mqttClient.setCallback(mqttCallback);
+ // _mqttClient.setCallback([this] (char* topic, byte* payload, unsigned int length) { this->callback(topic, payload, length); });
 
   _mqttClient.setClient(_ethClient);
   ethReconnect();
@@ -38,7 +39,7 @@ void KZGlan_mqtt::begin(String name,byte [] mac, IPAddress mqttHostIP, String mq
 void KZGlan_mqtt::ethReconnect()
 {
   DPRINTLN(F("KZGlan_mqtt::ethReconnect")); 
-  Ethernet.stop();
+ // Ethernet.stop();
   Ethernet.begin(_mac);
   //delay(500);
   DPRINTLN(getEthStatusString());
@@ -51,24 +52,29 @@ void KZGlan_mqtt::mqttReconnect()
     _mqttClient.disconnect();
     DPRINT(F("Proba polaczenia z MQTT .."));
     bool mqttconnStat=false;
-    if(_mqttUsr.length>0)
+    if(_mqttUsr.length()>0)
     {
-      mqttconnStat=_mqttClient.connect(_name.c_str(),_usr.c_str(),_pwd.c_str());
+      mqttconnStat=_mqttClient.connect(_name.c_str(),_mqttUsr.c_str(),_mqttPwd.c_str());
     }else
     {
       mqttconnStat=_mqttClient.connect(_name.c_str());
     }
     if(mqttconnStat) 
     {
+		DPRINTLN(F(" [ok]"));
+		DPRINT(F("Subskrypcja mqtt: "));
         _mqttClient.subscribe(_mojTopicIDSubscribe.c_str());
+		DPRINT(F("-- "));DPRINT(_mojTopicIDSubscribe);DPRINTLN(F("; "));
         _mqttClient.subscribe(_mojTopicIDSubscribeConfig.c_str());
-        _mqttClient.subscribe(_mojTopicIDPing.c_str());
-        _mqttClient.publish(_mojTopicIDPublishDebug.c_str(),"Polaczenie");
-        DPRINTLN(F(" [ok]"));
+        DPRINT(F("-- "));DPRINT(_mojTopicIDSubscribeConfig);DPRINTLN(F("; "));
+		_mqttClient.subscribe(_mojTopicIDPing.c_str());
+        DPRINT(F("-- "));DPRINT(_mojTopicIDPing);DPRINTLN(F("; "));
+		_mqttClient.publish(_mojTopicIDPublishDebug.c_str(),"Polaczenie");
+        
       }else
       {
         DPRINT(F("nieudane: "));
-        DPRINTLN(client.state());
+        DPRINTLN(_mqttClient.state());
       }
     }
 }
@@ -80,8 +86,8 @@ String KZGlan_mqtt::getEthStatusString()
   String statStr=""; 
   if(_ethClient.connected())
   {
-    IPAddress ip=_ethClient.localIP();
-    statStr="Eth connected: %d.%d.%d.%d\n"+ ip[0]+"."+ip[1]+"."+ip[2]+"."+ip[3];
+    IPAddress ip=Ethernet.localIP();
+    statStr="Eth connected: "+ String(ip[0])+"."+String(ip[1])+"."+String(ip[2])+"."+String(ip[3]);
     
   }else
   {
@@ -98,17 +104,17 @@ void KZGlan_mqtt::loop()
   /////////////////////////////// ETH  test connection ////////////////////////////////
   if(!_ethClient.connected())
   {
-    if( isEthNewDisconnecion)
+    if( _isEthNewDisconnecion)
     {
       DPRINTLN(F("*** -> Eth disconnected ***"));
       DPRINTLN(getEthStatusString());
-      isEthNewDisconnecion=false;
+      _isEthNewDisconnecion=false;
     }
-    if (now - lastEthReconnectAttempt > 5000) 
+    if (now - _lastEthReconnectAttempt > 5000) 
     {
-      lastEthReconnectAttempt = now;
+      _lastEthReconnectAttempt = now;
       DPRINTLN(F("*** Eth reconnecting.."));
-      isEthNewConnecion=true;
+      _isEthNewConnecion=true;
       ethReconnect();
     }
   } else
@@ -149,16 +155,16 @@ void KZGlan_mqtt::loop()
         hr=hr%24;
         
         char tmp[50];
-        sprintf ( tmp,"%s: %02lud%02lu:%02lu:%02lu ",_name,dd,hr, min,sec);
+        sprintf ( tmp,"%s: %02lud%02lu:%02lu:%02lu ",_name.c_str(),dd,hr, min,sec);
         
-        client.publish(_mojTopicIDPing.c_str(),tmp);
+        _mqttClient.publish(_mojTopicIDPing.c_str(),tmp);
       }  
     }
   }
 
 
 }
-void KZGlan_mqtt::callback(char* topic, byte* payload, unsigned int length) 
+void KZGlan_mqtt::callback(char* topic, uint8_t* payload, unsigned int length) 
 {
   String topicStr=String(topic);
   DPRINT(F("Topic:"));
@@ -177,15 +183,15 @@ void KZGlan_mqtt::callback(char* topic, byte* payload, unsigned int length)
   DPRINTLN(msgStr);
 
     
-  if(strcmp(_mojTopicIDPing,topicStr.c_str())==0) //powrocil komunikat ping
+  if(strcmp(_mojTopicIDPing.c_str(),topicStr.c_str())==0) //powrocil komunikat ping
   {
     _ostatniOdczytanyPingMqtt=millis();
     DPRINT(F("reveived ping at [ms]: "));DPRINTLN(_ostatniOdczytanyPingMqtt);
-  }else if(strcmp(_mojTopicIDSubscribeConfig,topicStr.c_str())==0)
+  }else if(strcmp(_mojTopicIDSubscribeConfig.c_str(),topicStr.c_str())==0)
   {
     DPRINT(F("reveived config msg: "));DPRINTLN(msgStr);
     
-  }else if(strcmp(_mojTopicIDSubscribe,topicStr.c_str())==0)
+  }else if(strcmp(_mojTopicIDSubscribe.c_str(),topicStr.c_str())==0)
   {
     _mqttCallback(topicStr,msgStr);
   }
--- KZGlan_mqtt/src/KZGlan_mqtt.h
+++ KZGlan_mqtt/src/KZGlan_mqtt.h
@@ -5,7 +5,7 @@
 #include <PubSubClient.h>
 
 
-//#define DEBUG_WIFI   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
+#define DEBUG_LAN_MQTT   //If you comment this line, the DPRINT & DPRINTLN lines are defined as blank.
 #ifdef DEBUG_LAN_MQTT    //Macros are usually in all capital letters.
   #define DPRINT(...)    Serial.print(__VA_ARGS__)     //DPRINT is a macro, debug print
   #define DPRINTLN(...)  Serial.println(__VA_ARGS__)   //DPRINTLN is a macro, debug print with new line
@@ -19,7 +19,7 @@ typedef void (*KZGlan_mqttCallback) (String topic, String msg);
 
 class KZGlan_mqtt
 {
-  byte _mac[]    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
+  byte _mac[6];//    = {  0xDE, 0xED, 0xBA, 0xFE, 0xFE, 0xED };
   EthernetClient _ethClient;
   PubSubClient _mqttClient;    
   unsigned long _ostatniOdczytanyPingMqtt=0;    // mqtt callback ustawia odczyt pingu
@@ -42,17 +42,19 @@ class KZGlan_mqtt
   String _mojTopicIDPublishDebug="DebugTopic/C1/"; // topic z komunikatami do serwera
   String _mojTopicIDPing="C1/Ping/";           //topic z komukatami keepalive
 
-  void callback(char* topic, byte* payload, unsigned int length);
+  void callback(char* topic, uint8_t* payload, unsigned int length);
   void ethReconnect();
   void mqttReconnect();
 
-  void KZGlan_mqttCallback _mqttCallback;
+  KZGlan_mqttCallback _mqttCallback;
+ //MQTT_CALLBACK_SIGNATURE _mqttCallback;
 public:
   KZGlan_mqtt(){};
-  void begin(String name,byte [] mac, IPAddress mqttHostIP, String mqttHost,String mqttUser,String mqttPwd, uint16_t mqttPort);
+  void begin(String name,byte * mac, IPAddress mqttHostIP, String mqttHost,String mqttUser,String mqttPwd, uint16_t mqttPort, KZGlan_mqttCallback mqttCallback);
   void loop();
   String getEthStatusString();
   void publish(String topic, String msg);
   void publishPrefix(String topic, String msg);
+  PubSubClient getMqttClient(){return _mqttClient;}
 };
 #endif
